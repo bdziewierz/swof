6af549c0f8b7ca6ead4946d32bb96c53b71d1e8c
Bogdan Dziewierz <bogdan@digital.crafts.agency>
2017-11-17 23:29:01 +0000

    Linter fixes, ES6

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
+"use strict";
+
 // We use serverless framework to manage our Lambda and API Gateway deployments on AWS
-const serverless = require('serverless-http');
+const serverless = require("serverless-http");
+
+const bodyParser = require("body-parser");
+
+const AWS = require("aws-sdk");
 
 // We use standard Express.js framework for web applications.
-const express = require('express');
+const express = require("express");
 const swof = express();
+swof.use(bodyParser.json({strict: false}));
 
-swof.get('/', function (req, res) {
-    res.send('Hello from Support Wheel of Fate!')
+const DEVELOPERS_TABLE = process.env.DEVELOPERS_TABLE;
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+// Returns two developers doing BAU in given day
+swof.get("/bau/:date", function (req, res) {
+    res.send("BAU for " + req.params.date + " stub.");
 });
 
 // We wrap the app in serverless-http for use in API Gateway
